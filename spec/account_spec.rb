c7212e90549f2092b7d7c43612f876cf253d0783
Eleanor Wintram <eleanorwintram@gmail.com>
2018-01-29 11:24:25 +0000

    account initializes with a balance of 0

---
+++ spec/account_spec.rb
@@ -0,0 +1,14 @@
+require 'account'
+
+describe Account do
+
+  subject(:account) { described_class.new }
+
+  describe "#initialize" do
+
+    it "initializes with a balance of 0" do
+      expect(account.balance).to eq 0
+    end
+  end
+
+end
