cee01b0eaf5453abcd06d57664d906018d54fe18
Eleanor Wintram <eleanorwintram@gmail.com>
2018-01-29 11:28:21 +0000

    account initializes with an empty array of transactions

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,6 +9,10 @@ describe Account do
     it "initializes with a balance of 0" do
       expect(account.balance).to eq 0
     end
+
+    it "initializes with an empty array of transactions" do
+      expect(account.transactions).to be_empty
+    end
   end
 
 end
